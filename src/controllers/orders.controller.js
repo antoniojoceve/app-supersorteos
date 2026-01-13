d5c9605a5902519bb05feef362e38b616dd26b5c
antoniojoceve <antoniofebrero24@hotmail.com>
2026-01-13 04:38:07 +0100

    fix: orders name table

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -3,23 +3,23 @@ const pool = require("../config/db");
 const getAllOrders = async (req, res) => {
   try {
     const result = await pool.query(`
-      SELECT 
+      SELECT
         o.id,
-        r.title AS raffle,
-        o.number,
-        u.email AS user,
-        o.status,
+        r.title        AS raffle_title,
+        u.email        AS user_email,
+        o.total_amount,
+        o.payment_status,
         o.created_at
       FROM orders o
       JOIN raffles r ON r.id = o.raffle_id
-      JOIN users u ON u.id = o.user_id
+      JOIN users u   ON u.id = o.user_id
       ORDER BY o.created_at DESC
     `);
 
     res.json(result.rows);
   } catch (err) {
     console.error("ORDERS ERROR:", err);
-    res.status(500).json({ error: "Error obteniendo pedidos" });
+    res.status(500).json({ error: err.message });
   }
 };
 
