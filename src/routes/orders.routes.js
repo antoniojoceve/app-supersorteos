bf64819a02285d5bdfd5eac8c038f8ed3a8c4adf
antoniojoceve <antoniofebrero24@hotmail.com>
2026-01-23 02:29:16 +0100

    fix routes orders.routes.js

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { ordersLimiter } = require("../middleware/rateLimit");
+const { ordersLimiter } = require("../middlewares/rateLimit");
 
 const {
   getAllOrders,
